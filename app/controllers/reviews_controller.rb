7d8cc6397125a62979eb1141e59d1ac899b57b0e
awais hassan <awais.hassan.k@gmail.com>
2017-02-02 11:55:40 +0500

    Integrated Bootstrap and style all pages.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,6 +26,7 @@ class ReviewsController < ApplicationController
   def create
     @product=Product.find(params[:product_id])
     @review = @product.reviews.create(review_params)
+    
 
     redirect_to product_path (@product)
 
